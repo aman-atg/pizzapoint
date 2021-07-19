1801480203f018387b7c87c9ba790e973a18055b
Aman Ansari <amanansari.a2@gmail.com>
2021-07-19 23:40:00 +0530

    add drag to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,6 +27,9 @@ const Header = () => {
     <header>
       <div className="logo">
         <motion.svg
+          drag
+          dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+          dragElastic={1}
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
